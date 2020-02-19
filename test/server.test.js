59739058908e873fc89b851045fc75790ca538b8
Jose <jozylopez27@gmail.com>
2020-02-19 15:38:06 -0600

    tests

--- test/server.test.js
+++ test/server.test.js
@@ -1,3 +1,25 @@
-const { expect } = require('chai')
-const supertest = require('supertest')
+const expect = require('chai').expect
+const request = require('supertest')
 const app = require('../server')
+
+describe('Get /movie', () => {
+    it('should return an array of movies', () => {
+        return request(app)
+        .get('/movie')
+        .expect(200)
+        .expect('Content-type', /json/)
+        .then(res => {
+            expect(res.body).to.be.an('array');
+            expect(res.body).to.have.lengthOf.at.least(1);
+        })
+    });
+
+    const params = ['genre', 'country', 'avg_vote']
+
+    // it(`should return 400 if ${params} query is invalid`, () => {
+    //     return request(app)
+
+    // });
+
+    it(`should return an array of movies filtered buy either ${params}`);
+})
